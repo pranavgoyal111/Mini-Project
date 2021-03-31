dded42d093820b6da2f5f6be686fe31269620bd4
Pranav Goyal <76400951+pranavgoyal111@users.noreply.github.com>
2021-03-31 19:46:00 +0530

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-var dict={};
-function onSubmit(){
-    var name = document.getElementById("name").value;
-    console.log(name)
-
-    var details = document.getElementById("details");
-    console.log(details)
-}
