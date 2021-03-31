dd8715948bb1e0a2eacb4f03dd8ac73977153516
Pranav Goyal <76400951+pranavgoyal111@users.noreply.github.com>
2021-03-31 17:36:01 +0530

    Front-End Files

---
+++ index.js
@@ -0,0 +1,8 @@
+var dict={};
+function onSubmit(){
+    var name = document.getElementById("name").value;
+    console.log(name)
+
+    var details = document.getElementById("details");
+    console.log(details)
+}
